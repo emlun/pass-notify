9fc37c2fa5f7f008be6bd2496ccb34e82af419fe
Emil Lundberg <emil@yubico.com>
2021-08-17 17:36:08 +0200

    Add support for wtype in passmenu-notify

--- passmenu/passmenu-notify.bash
+++ passmenu/passmenu-notify.bash
@@ -6,13 +6,15 @@ PROGRAM="${0##*/}"
 
 typeit=0
 notify=0
+type_cmd='xdotool type --clearmodifiers --file -'
 
-opts="$(getopt -o "" -l notify,typeit -n "$PROGRAM" -- "$@")"
-[[ $? -ne 0 ]] && (echo "Usage: $PROGRAM [--notify] [--typeit]" >&2 ; exit 1)
+opts="$(getopt -o "" -l notify,typeit,typeit-wtype -n "$PROGRAM" -- "$@")"
+[[ $? -ne 0 ]] && (echo "Usage: $PROGRAM [--notify] [--typeit] [--typeit-wtype]" >&2 ; exit 1)
 eval set -- "$opts"
 while true; do case $1 in
 	--notify) notify=1; shift ;;
 	--typeit) typeit=1; shift ;;
+	--typeit-wtype) typeit=1; type_cmd='wtype -'; shift ;;
 	--) shift; break ;;
 esac done
 
@@ -32,7 +34,8 @@ if [[ $typeit -eq 0 ]]; then
 		pass show -c "$password" 2>/dev/null
 	fi
 else
+
 	pass show "$password" |
 		awk 'BEGIN{ORS=""} {print; exit}' |
-		xdotool type --clearmodifiers --file -
+		$type_cmd
 fi
