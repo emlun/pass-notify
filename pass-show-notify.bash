8026c3153d8e02a24bff4ff75b9f26043c53fe27
Emil Lundberg <emil@yubico.com>
2022-05-29 17:03:14 +0200

    Show different output in `pass-show-notify` on failure

--- pass-show-notify.bash
+++ pass-show-notify.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-output=$(cmd_show "$@" | head -n1)
 
-notify-send -a "${PROGRAM}" -u normal -t 2000 "Password copied" "$output"
+
+if output=$(cmd_show "$@" | head -n1); then
+  notify-send -a "${PROGRAM}" -u normal -t 2000 "Password copied" "$output"
+else
+  notify-send -a "${PROGRAM}" -u critical -t 5000 "Failed to copy password" "$output"
+fi
